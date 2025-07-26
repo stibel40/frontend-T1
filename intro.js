7f65d9be820750039c884c3bffc483a9feca82dd
stibel <stibelbaena1991@outlook.com>
2025-07-26 13:08:03 -0500

    inicio proyecto

---
+++ intro.js
@@ -0,0 +1,8 @@
+alert("hola desde forma externa");
+
+let number = 10
+alert("el numero es:"+ number)
+let respuesta = prompt ("eres mayor de edad?")
+alert ("tu respuesta es: " + respuesta)
+console.log("tu respuesta es:" + respuesta)
+document.write("tu respuesta es :" + respuesta)
